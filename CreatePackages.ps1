dacf733d1997b8746a97bf5d4136b4bed84415df
Jonathan Ruckert <jruckert@novaworks.com.au>
2017-01-11 10:56:47 +1100

    Fixing Packaging

--- CreatePackages.ps1
+++ CreatePackages.ps1
@@ -2,9 +2,9 @@ gci -Recurse "src\**\project.json" |
 Foreach-Object {
   if($Env:PackageType -eq "development")
   {
-     dotnet pack -c %CONFIGURATION% --no-build --version-suffix %LABEL% -o artifacts $_.DirectoryName
+     dotnet pack -c $Env:CONFIGURATION --no-build --version-suffix $Env:LABEL -o artifacts $_.DirectoryName
   }
   else{
-	 dotnet pack -c %CONFIGURATION% --no-build -o artifacts $_.DirectoryName
+	 dotnet pack -c $Env:CONFIGURATION --no-build -o artifacts $_.DirectoryName
   }
 }
