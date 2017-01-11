0b9cddd34e24ac08d78e7065b08c835671adabc8
Jonathan Ruckert <jruckert@novaworks.com.au>
2017-01-11 11:34:00 +1100

    Fixing Powershell to save back version number when releasing

--- ReleaseVersion.ps1
+++ ReleaseVersion.ps1
@@ -6,7 +6,7 @@ if($Env:APPVEYOR_REPO_TAG -eq 'true') { # if there is a repo tag pushed
 	Foreach-Object {
 		$json = Get-Content -Raw -Path $_.FullName | ConvertFrom-Json
 		$json.version = $Env:APPVEYOR_REPO_TAG_NAME.TrimStart("v")
-		$json | ConvertTo-Json -depth 999 | Out-File $jsonpath
+		$json | ConvertTo-Json -depth 999 | Out-File $_.FullName
 	}
   }
 }
